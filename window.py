047eaa0bd2610b128760bc2ca65bd901b498db74
Matyrela <mativaresc@gmail.com>
2022-06-19 20:36:12 -0300

    windows.py

---
+++ window.py
@@ -0,0 +1,35 @@
+import pygame
+from screeninfo import get_monitors
+size = []
+for m in get_monitors():
+    print(m)
+    print("......")
+
+
+background_colour = (234, 212, 252)
+
+# Define the dimensions of
+# screen object(width,height)
+screen = pygame.display.set_mode((300, 300))
+
+# Set the caption of the screen
+pygame.display.set_caption('UFGMWG')
+
+# Fill the background colour to the screen
+screen.fill(background_colour)
+
+# Update the display using flip
+pygame.display.flip()
+
+# Variable to keep our game loop running
+running = True
+
+# game loop
+while running:
+
+    # for loop through the event queue
+    for event in pygame.event.get():
+
+        # Check for QUIT event
+        if event.type == pygame.QUIT:
+            running = False
